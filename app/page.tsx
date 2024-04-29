88dc122bc3cef02993bfb3fc5707b9820c0e6187
HASSNAIN ZIA <hassnainzia.dev121@gmail.com>
2024-04-29 14:19:37 +0500

    feat-page-ui design

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
 export default function Home() {
-  return <div className="bg-black">{/* Hero section goes here */}</div>;
+  return <div className="flex h-screen items-center justify-center p-2 text-center md:p-4 lg:p-8 bg-primary-600 ">
+    <div>
+      <div className=' text-zinc-700'>
+        <p>Community awaits you</p>
+        <h3>Activate Your Message Community</h3>
+        <p>Physiological respiration involves the mechanisms that ensure that the composition of the functional residual capacity is kept constant, and equilibrates with the gases dissolved in the pulmonary capillary blood, and thus throughout the body.</p>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  </div>;
 }
