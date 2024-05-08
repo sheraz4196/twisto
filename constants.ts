a1f62a7ec87d86102eeb0f553aa4a61179327081
sheraz4196 <sheraztoor321@gmail.com>
2024-05-08 11:50:36 -0700

    feat: theme names

--- constants.ts
+++ constants.ts
@@ -1,1 +1,13 @@
 export const CVTemplates: string[] = ['Pedro', 'ClassicI'];
+export const CVThemes: string[] = [
+  'Azure Aura',
+  'Crimson Crest',
+  'Indigo Influence',
+  'Sapphire Spectra',
+  'Emerald Eclipse',
+  'Graphite Glimmer',
+  'Ruby Radiance',
+  'Topaz Terra',
+  'Amber Aether',
+  'Opal Oasis',
+];
