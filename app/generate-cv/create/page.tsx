d36f53d98d14374f805992396de14a22bc32c296
sheraz4196 <sheraztoor321@gmail.com>
2024-05-08 10:59:02 -0700

    feat: Pedro to show real time response

--- app/generate-cv/create/page.tsx
+++ app/generate-cv/create/page.tsx
@@ -1,11 +1,12 @@
 import ClassicOne from '@/components/cv-designes/classic-one';
+import Pedro from '@/components/cv-designes/pedro';
 import CreateCVForm from '@/components/forms/create-cv-form';
 
 export default function CreateCV() {
   return (
     <div className="grid grid-cols-3">
       <CreateCVForm />
-      <ClassicOne />
+      <Pedro />
     </div>
   );
 }
