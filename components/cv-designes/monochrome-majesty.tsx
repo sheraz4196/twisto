e236199aab9d843e40a5e0bfcbb554ad42946ef8
sheraz4196 <sheraztoor321@gmail.com>
2024-05-09 03:20:07 -0700

    feat: initialized new template setup

--- components/cv-designes/monochrome-majesty.tsx
+++ components/cv-designes/monochrome-majesty.tsx
@@ -1,12 +1,12 @@
 'use client';
 import { forwardRef } from 'react';
 
-const Pedro = forwardRef<HTMLDivElement>((props, ref) => {
+const MonochromeMajesty = forwardRef<HTMLDivElement>((props, ref) => {
   return (
     <section className="col-span-2 flex" id="classic-one" ref={ref}>
       {/* New Template Goes here */}
     </section>
   );
 });
-Pedro.displayName = 'Pedro';
-export default Pedro;
+MonochromeMajesty.displayName = 'MonochromeMajesty';
+export default MonochromeMajesty;
